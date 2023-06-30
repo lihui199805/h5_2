5943d7f50466dfd3ed6649a29bb85929a508b378
冠希哥没腿毛 <12217742+skjhghjksgdfkjsdfhiujwsh@user.noreply.gitee.com>
2023-06-30 11:47:00 +0800

    h5_2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  lintOnSave:false,
+  configureWebpack:{
+    devServer:{
+      host:"localhost",
+      port:8088,
+      proxy:{
+        '/api':{
+          target:"http://localhost:7001",
+          changeOrigin:true,
+          ws:true,
+          pathRewrite:{
+            "^/api":""
+          }
+        }
+      }
+    }
+  }
+  
 });
